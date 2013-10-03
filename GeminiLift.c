6d9c62f2c4c90e44f7fbf7e3c0294cb01dd958b5
EngineeringAwesome <EngineeringAwesome@users.noreply.github.com>
2013-10-03 14:06:57 -0500

    Added blocks of blank code to set the lift to various positions

--- GeminiLift.c
+++ GeminiLift.c
@@ -1,1 +1,30 @@
 #pragma systemFile
+
+liftCollect() {
+  //set the lift to the collection position
+}
+
+liftDump() {
+  //set the lift to the dumping position
+  //need different cases, or code for pendulum heights?
+}
+
+liftStorage() {
+  //set the lift to the storage position
+}
+
+liftIncrementPosition(int value) {
+  //increments the current position of the lift by the given value
+}
+
+liftDecrementPosition(int value) {
+  //decrements the current position of the lift by the given value
+}
+
+liftUpSpeed(int speed){
+  //moves the lift up at a given speed
+}
+
+liftDownSpeed(int speed) {
+  //moves the lift down at a given speed
+}
