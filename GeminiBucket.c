2f007586314c276046b13936cf55ff305d221085
EngineeringAwesome <EngineeringAwesome@users.noreply.github.com>
2013-10-03 14:02:02 -0500

    Added blocks of blank code to set the bucket to various positions

--- GeminiBucket.c
+++ GeminiBucket.c
@@ -1,1 +1,33 @@
 #pragma systemFile
+
+bucketCollect() {
+  //set the bucket to collection position for scooping up blocks
+}
+
+bucketStorage() {
+  //set the bucket to the storage position
+}
+
+bucketCarry() {
+  //set the bucket to the carrying position to keep blocks in
+}
+
+bucketFloat() {
+  //turn off power to the servo motor to allow the bucket to float
+}
+
+bucketDump() {
+  //set the bucket to the dumping position
+}
+
+bucketIncrementPosition(int value) {
+  //increment the current position by a given value
+  //Servo[bucket] += value;
+  //check for bounds
+}
+
+bucketDecrementPosition(int value) {
+  //decrements the current position by a given value
+  //Servo[bucket] -= value;
+  //check for bounds
+}
