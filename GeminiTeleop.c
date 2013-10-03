ff740f597798ae02cef300decae92f0a3f83efc2
EngineeringAwesome <EngineeringAwesome@users.noreply.github.com>
2013-10-03 13:44:06 -0500

    Initial Commit
    Added basic code that matches the competition template

---
+++ GeminiTeleop.c
@@ -0,0 +1,22 @@
+#include "GeminiDrivetrain.c"
+#include "GeminiLift.c"
+#include "GeminiBucket.c"
+
+#include "JoystickDriver.c"
+
+
+void initializeRobot() {
+
+  return;
+}
+
+task main() {
+  initializeRobot();
+  
+  waitForStart();
+  
+  while(true) {
+    getJoystickSettings(joystick);
+  
+  }
+}
