2ea1d5bb7668d56e790e97270ec4666a491ba954
EngineeringAwesome <EngineeringAwesome@users.noreply.github.com>
2013-10-03 13:45:35 -0500

    Initial Commit
    Added basic code that matches the default Autonomous Template

---
+++ GeminiAutonomous.c
@@ -0,0 +1,21 @@
+#include "GeminiDrivetrain.c"
+#include "GeminiLift.c"
+#include "GeminiBucket.c"
+
+#include "JoystickDriver.c"
+
+
+void initializeRobot() {
+
+  return;
+}
+
+task main() {
+  initializeRobot();
+  
+  waitForStart();
+  
+  while(true) {
+  
+  }
+}
