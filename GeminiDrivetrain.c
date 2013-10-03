87e4262a81a1b0df51933557f2b20289216b53dc
EngineeringAwesome <EngineeringAwesome@users.noreply.github.com>
2013-10-03 13:52:57 -0500

    Added functions to control left and right side motors, and functions to drive forward and backward

--- GeminiDrivetrain.c
+++ GeminiDrivetrain.c
@@ -1,1 +1,37 @@
 #pragma systemFile
+
+setLeftSideSpeed(int speed) {
+  
+}
+
+setRightSideSpeed(int speed) {
+  
+}
+
+setLeftSidePower(int power) {
+  
+}
+
+setRightSidePower(int power) {
+  
+}
+
+driveForwardSpeed(int speed) {
+  setLeftSideSpeed(speed);
+  setRightSideSpeed(speed);
+}
+
+driveForwardPower(int power) {
+  setLeftSidePower(power);
+  setRightSidePower(power);
+}
+
+driveBackwardSpeed(int speed) {
+  setLeftSideSpeed(speed);
+  setRightSideSpeed(speed);
+}
+
+driveBackwardPower(int power) {
+  setLeftSidePower(power);
+  setRightSidePower(power);
+}
