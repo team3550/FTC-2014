f649ccda29a89774ee54a6b21165c8fc59fd1e19
EngineeringAwesome <EngineeringAwesome@users.noreply.github.com>
2013-10-15 01:03:58 -0500

    Added some pseudo code to select a list of items from a menu

---
+++ GeminiUserInterface.c
@@ -0,0 +1,13 @@
+//code to display things on the NXT display
+
+/*  TODO: fix array input
+void displayMenuItems(string items[]) {
+  //displays a list of items, defined by the input list
+}
+*/
+
+int displaySelectMenuItems(string items[]) {
+  displayMenuItems(string items[]);
+  //let the user select a specific item
+  return itemNumber;
+}
