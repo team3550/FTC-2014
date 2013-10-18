a27ec363fb27849bac23388f84c238745c12410b
EngineeringAwesome <EngineeringAwesome@users.noreply.github.com>
2013-10-18 09:53:32 -0500

    added code to set drive motors at speed and power

--- GeminiDrivetrain.c
+++ GeminiDrivetrain.c
@@ -1,19 +1,31 @@
 #pragma systemFile
 
 setLeftSideSpeed(int speed) {
-  
+  //need to add code to set motors to speed
+  motor[leftDriveA] = speed;
+  motor[leftDriveB] = speed;
+  motor[leftDriveC] = speed;
 }
 
 setRightSideSpeed(int speed) {
-  
+  //need to add code to set motors to speed
+  motor[rightDriveA] = speed;
+  motor[rightDriveB] = speed;
+  motor[rightDriveC] = speed;
 }
 
 setLeftSidePower(int power) {
-  
+  //need to add code to set motors to power
+  motor[leftDriveA] = power;
+  motor[leftDriveB] = power;
+  motor[leftDriveC] = power;
 }
 
 setRightSidePower(int power) {
-  
+  //need to add code to set motors to power
+  motor[rightDriveA] = power;
+  motor[rightDriveB] = power;
+  motor[rightDriveC] = power;
 }
 
 driveForwardSpeed(int speed) {
