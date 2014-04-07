93793569a52df6f27f8dcebb9e2314de599a785f
EngineeringAwesome <robomower9508@gmail.com>
2014-04-07 15:56:32 -0500

    cleanup

--- GeminiDrivetrain.c
+++ GeminiDrivetrain.c
@@ -1,7 +1,7 @@
 #pragma systemFile
 
 #ifndef __GEMINI_DRIVERS__
-#include "gemini-drivers.c"
+#include "gemini-drivers.h"
 #endif
 
 //========== DEFINES ==========
@@ -13,50 +13,50 @@
 //========== functions ==========
 
 
-setLeftSideSpeed(int speed) {
+void setLeftSideSpeed(int speed) {
   //need to add code to set motors to speed
   motor[leftDriveA] = speed;
   motor[leftDriveB] = speed;
   motor[leftDriveC] = speed;
 }
 
-setRightSideSpeed(int speed) {
+void setRightSideSpeed(int speed) {
   //need to add code to set motors to speed
   motor[rightDriveA] = speed;
   motor[rightDriveB] = speed;
   motor[rightDriveC] = speed;
 }
 
-setLeftSidePower(int power) {
+void setLeftSidePower(int power) {
   //need to add code to set motors to power
   motor[leftDriveA] = power;
   motor[leftDriveB] = power;
   motor[leftDriveC] = power;
 }
 
-setRightSidePower(int power) {
+void setRightSidePower(int power) {
   //need to add code to set motors to power
   motor[rightDriveA] = power;
   motor[rightDriveB] = power;
   motor[rightDriveC] = power;
 }
 
-driveForwardSpeed(int speed) {
+void driveForwardSpeed(int speed) {
   setLeftSideSpeed(speed);
   setRightSideSpeed(speed);
 }
 
-driveForwardPower(int power) {
+void driveForwardPower(int power) {
   setLeftSidePower(power);
   setRightSidePower(power);
 }
 
-driveBackwardSpeed(int speed) {
+void driveBackwardSpeed(int speed) {
   setLeftSideSpeed(speed);
   setRightSideSpeed(speed);
 }
 
-driveBackwardPower(int power) {
+void driveBackwardPower(int power) {
   setLeftSidePower(power);
   setRightSidePower(power);
 }
