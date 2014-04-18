019808c345328ed7d2b7f59952af8b0a5c04ed98
EngineeringAwesome <robomower9508@gmail.com>
2014-04-18 17:24:42 -0500

    added prompt auto delay

--- GeminiUserInterface.c
+++ GeminiUserInterface.c
@@ -2,12 +2,42 @@
 
 
 //========== defines ==========
+#define NXT_BUTTON_ENTER  2
+#define NXT_BUTTON_LEFT   2
+#define NXT_BUTTON_RIGHT  2
+#define NXT_BUTTON_BACK   2
 
 //========== prototypes ==========
-void promptTimeDelay(int max = 30);
+void promptTimeDelay(int maxTime = 30000);
 void promptAutonomousMode();
 
 //========== variables ==========
+long autoDelayTime = 0;
+long autoDelayTimeStep = 1000;
 string autoMode = "";
 
 //========== functions ==========
+void promptTimeDelay(int maxTime) {
+  eraseDisplay();
+  nxtDisplayCenteredTextLine(3, "Delay Time:");
+  nxtDisplayCenteredTextLine(8, "- Dec.     Inc. +");
+
+  int nNxtButtonPressedOld = -1;
+  while (nNxtButtonPressed != NXT_BUTTON_ENTER) {
+
+    if ((nNxtButtonPressed == NXT_BUTTON_LEFT) && (nNxtButtonPressedOld != NXT_BUTTON_LEFT)) {
+      if ((autoDelayTime - autoDelayTimeStep) >= 0) {
+        autoDelayTime -= autoDelayTimeStep;
+      } else {
+        autoDelayTime = 0;
+      }
+    } else if ((nNxtButtonPressed == NXT_BUTTON_LEFT) && (nNxtButtonPressedOld != NXT_BUTTON_LEFT)) {
+      if ((autoDelayTime + autoDelayTimeStep) <= maxTime) {
+        autoDelayTime += autoDelayTimeStep;
+      } else {
+        autoDelayTime = maxTime;
+      }
+    }
+    nxtDisplayCenteredTextLine(6, "%f Sec.", (autoDelayTime/1000));
+  }
+}
