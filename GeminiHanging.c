f61c680b70247350ee796e2ebab65e50bcf821b3
EngineeringAwesome <robomower9508@gmail.com>
2014-04-17 20:21:05 -0500

    added prototypes and function shells

--- GeminiHanging.c
+++ GeminiHanging.c
@@ -3,7 +3,42 @@
 //========== defines ==========
 
 //========== prototypes ==========
+void hangingInitialize();
 
+void hangingPTOEngage();
+void hangingPTODisengage();
+
+void hangingRatchetEngage();
+void hangingRatchetDisengage();
+void hangingRatchetLock();
+void hangingRatchetUnlock();
 //========== variables ==========
 
 //========== functions ==========
+void hangingInitialize() {  //initialze the hanging system
+
+}
+
+void hangingPTOEngage() { //Engage the PTO from the drivetrain
+
+}
+
+void hangingPTODisengage() {  //disngage the PTO from the drivetrain
+
+}
+
+void hangingRatchetEngage() {  //engage the ratchet to the hanging shaft
+
+}
+
+void hangingRatchetDisengage() {  //disengage the ratchet from the hanging shaft
+
+}
+
+void hangingRatchetLock() {       //Lock the ratchet by backdriving slightly
+
+}
+
+void hangingRatchetUnlock() {     //Unlock the ratchet by releasing pressure from the ratchet system
+
+}
